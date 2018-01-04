2aaf2358abb2c5dc0c7aed26b70abf3517443fc4
Roland Averkamp <roland.averkamp.0@gmail.com>
2018-01-04 14:56:54 +0100

    static pages again visible, refactor

--- src/spinat/jettyprism/Main.java
+++ src/spinat/jettyprism/Main.java
@@ -22,15 +22,17 @@ import java.io.FileReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Locale;
 
 import org.apache.log4j.PropertyConfigurator;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
@@ -48,8 +50,7 @@ public class Main {
         }
         String propFileName = args[0];
         java.util.Properties props = new java.util.Properties();
-        File pf = new File(propFileName);
-        File apf = pf.getAbsoluteFile();
+        File apf = new File(propFileName).getAbsoluteFile();
         if (!apf.canRead()) {
             throw new RuntimeException("can not read file: " + apf);
         }
@@ -99,19 +100,40 @@ public class Main {
         // or programmatically obtain it for use in test cases.
         
         HandlerList handlers = new HandlerList();
+        
+        Handler dadHandler = createDADHandler(props, apf);
+           
+        ArrayList<Handler> l = createStaticHandler(props);
+        for(Handler h : l) {
+            handlers.addHandler(h);
+        }
+        // if dadHandler is added first, the static handlers are not visible
+        handlers.addHandler(dadHandler);
         server.setHandler(handlers);
-
-        // The ServletHandler is a dead simple way to create a context handler that is backed by an instance of a
-        // Servlet.  This handler then needs to be registered with the Server object.
-        ServletHandler handler = new ServletHandler();
+        // Start things up! By using the server.join() the server thread will join with the current thread.
+        // See "http://docs.oracle.com/javase/1.5.0/docs/api/java/lang/Thread.html#join()" for more details.
+        try {
+            server.start();
+        } catch (java.net.BindException bex) {
+            msg("ERROR: the port " + intPort + " is already in use. Exiting ...");
+            System.exit(1);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            msg("Exiting ...");
+            System.exit(1);
+        }
+        server.join();
+    }
+    
+    static Handler createDADHandler(java.util.Properties props, File propertyFile) {
+        ServletContextHandler handler = new ServletContextHandler();
         // Passing in the class for the servlet allows jetty to instantite an instance of that servlet and mount it
         // on a given context path.
 
         // !! This is a raw Servlet, not a servlet that has been configured through a web.xml or anything like that !!
         ServletHolder holder = new ServletHolder(com.prism.ServletWrapper.class);
         
-        //String prismconf = propFileName; //props.getProperty("prismconf", "prism.xconf");
-        File pc = apf; //new File(prismconf).getAbsoluteFile();
+        File pc = propertyFile; //new File(prismconf).getAbsoluteFile();
         if (pc.canRead()) {
             holder.setInitParameter("properties", pc.toString());
         } else {
@@ -120,9 +142,12 @@ public class Main {
         }
         msg("dbprism config " + pc.toString());
         String dads = props.getProperty("dads", "/dads");
-        handler.addServletWithMapping(holder, dads + "/*");
-        handlers.addHandler(handler);
-        
+        handler.addServlet(holder, dads + "/*");
+        return handler;
+    }
+    
+    static ArrayList<Handler> createStaticHandler(java.util.Properties props) {
+        ArrayList<Handler> res = new ArrayList<>();
         HashSet<String> set = new HashSet<>();
         for (String s : props.stringPropertyNames()) {
             if (s.startsWith("static.")) {
@@ -152,13 +177,16 @@ public class Main {
             if (!f.isDirectory()) {
                 throw new RuntimeException("not a dir " + f);
             }
+            String targetDir = f.getAbsolutePath();
+            
             String mount = props.getProperty(prefix + "wdir", "");
             if (mount.equals("")) {
                 continue;
             }
+            
             ResourceHandler resource_handler = new ResourceHandler();
             resource_handler.setDirectoriesListed(true);
-            resource_handler.setResourceBase(f.toString());
+            resource_handler.setResourceBase(targetDir);
             
             String cc = props.getProperty(prefix + "cache-control", "");
             if (!cc.equals("")) {
@@ -166,23 +194,11 @@ public class Main {
             }
             ContextHandler contextHandler = new ContextHandler(mount);
             contextHandler.setHandler(resource_handler);
-            msg("adding static dir " + mount + " -> " + f.toString());
-            handlers.addHandler(contextHandler);
+            msg("adding static dir " + mount + " -> " + targetDir);
+            res.add(contextHandler);
         }
-        server.setHandler(handlers);
-        // Start things up! By using the server.join() the server thread will join with the current thread.
-        // See "http://docs.oracle.com/javase/1.5.0/docs/api/java/lang/Thread.html#join()" for more details.
-        try {
-            server.start();
-        } catch (java.net.BindException bex) {
-            msg("ERROR: the port " + intPort + " is already in use. Exiting ...");
-            System.exit(1);
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-            msg("Exiting ...");
-            System.exit(1);
-        }
-        server.join();
+        return res;
+        
     }
     
 }
